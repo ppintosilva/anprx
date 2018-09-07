8bbc560d9cffb71689a6cc89060f6fe6affc1c2b
PedrosWits <ppintodasilva@gmail.com>
2018-09-07 19:38:38 +0100

    Added utils and dummy source file to simulate and test skeleton of project

---
+++ anprx/dummy.py
@@ -0,0 +1,18 @@
+def dummy_equals(dummy1, dummy2):
+    """
+    Yet another dummy function.
+
+    Parameters
+    ----------
+    dummy1 : int
+        The first dummy parameter.
+    dummy2 : int
+        The second dummy parameter.
+
+    Returns
+    -------
+    bool
+        True if dummy1 equals dummy2, False otherwise.
+
+    """
+    return dummy1 == dummy2
