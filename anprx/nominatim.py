590d3272a8ce52de24e94ea6eba947ae71b691c1
PedrosWits <ppintodasilva@gmail.com>
2018-09-09 23:58:12 +0100

    Added method to get ways given address from OSM

--- anprx/nominatim.py
+++ anprx/nominatim.py
@@ -7,17 +7,28 @@
 ################################################################################
 
 import osmnx as ox
-from collections import namedtuple
+from collections import OrderedDict
 
-Test = namedtuple(
-    'Test',
-    [
-        'lat',
-        'lng'
-    ])
+def lookup_ways(address):
+    """
+    Find the OpenStreetMap ways that match a given address.
 
-def lookup_address(point):
-    pass
+    Parameters
+    ----------
+    address:
+        Address for reverse geocoding
 
-def lookup_addresses(points):
-    pass
+    Returns
+    -------
+    osm_ids :
+        List of osm ids for ways that match the given address query
+    """
+
+    params = OrderedDict()
+    params['format'] = "json"
+    params['address_details'] = 0
+    params['q'] = address
+
+    response_json = ox.nominatim_request(params = params)
+
+    return list(map(lambda x: x["osm_id"], filter(lambda x: x['osm_type'] == "way", response_json)))
