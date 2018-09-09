556a28a61f94b12c5f516826785ede2420826a8a
PedrosWits <ppintodasilva@gmail.com>
2018-09-09 23:58:34 +0100

    Added tests for method to compute ways given address from OSM

---
+++ tests/test_nominatim.py
@@ -0,0 +1,16 @@
+import anprx
+import pytest
+
+def test_nominatim_lookup_ways():
+    expected = \
+        ['37899441',
+         '461119586',
+         '4725926',
+         '4692270',
+         '4655478',
+         '2544439',
+         '31992849']
+
+    result = anprx.lookup_ways("Newcastle A186 Westgate Rd")
+
+    assert expected == result
