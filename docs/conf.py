0cf230af514f54e96de18ae3c4a42a465038e297
PedrosWits <ppintodasilva@gmail.com>
2018-09-09 12:54:47 +0100

    Removed numpydoc extension which was causing a headaching bug - conflicting with standard autodoc on namedtuples. Added methods to delete line 'Alias of' when documenting members of namedtuple

--- docs/conf.py
+++ docs/conf.py
@@ -52,8 +52,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.napoleon',
     'sphinx.ext.todo',
-    'sphinx.ext.viewcode',
-    'numpydoc'
+    'sphinx.ext.viewcode'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -171,3 +170,19 @@ texinfo_documents = [
 
 
 # -- Extension configuration -------------------------------------------------
+
+def no_namedtuple_attrib_docstring(app, what, name,
+                                   obj, options, lines):
+    is_namedtuple_docstring = (
+        len(lines) == 1 and
+        lines[0].startswith('Alias for field number')
+    )
+    if is_namedtuple_docstring:
+        # We don't return, so we need to purge in-place
+        del lines[:]
+
+def setup(app):
+    app.connect(
+        'autodoc-process-docstring',
+        no_namedtuple_attrib_docstring,
+    )
