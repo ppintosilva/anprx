16c230c17a1d551899e6fa07840ed7d6edb997f4
PedrosWits <ppintodasilva@gmail.com>
2018-09-10 17:12:17 +0100

    Added helpers to retrieve edges that match a given attribute set

--- anprx/helpers.py
+++ anprx/helpers.py
@@ -6,7 +6,9 @@
 # Web: https://github.com/pedroswits/anprx
 ################################################################################
 
+import networkx as nx
 from .network import *
+from .constants import *
 
 def points_from_lists(latitudes, longitudes):
     """
@@ -82,3 +84,90 @@ def longitudes_from_points(points):
         List[float]
     """
     return [ point.lng for point in points ]
+
+###
+###
+
+def edges_with_at_least_one_property(network, properties):
+    """
+    Find edges that match at least once in all property sets: (key, values)
+
+    Parameters
+    ---------
+    network : nx.MultiDiGraph
+        a street network
+
+    properties : dict(str : set)
+        properties of edges to filter by
+
+    Returns
+    -------
+    generator
+        generator of edges (u,v)
+    """
+    for u,v,d in network.edges(data = True):
+        for key, values in properties.items():
+            if d[key] in values:
+                yield (u,v)
+
+###
+###
+
+def edges_with_all_properties(network, properties):
+    """
+    Find edges that match always in all property sets: (key, values)
+
+    Parameters
+    ---------
+    network : nx.MultiDiGraph
+        a street network
+
+    properties : dict(str : set)
+        properties of edges to filter by
+
+    Returns
+    -------
+    generator
+        generator of edges (u,v)
+    """
+    for u,v,d in network.edges(data = True):
+        nmatches = 0
+        for key, values in properties.items():
+            if d[key] in values:
+                nmatches = nmatches + 1
+            else:
+                break
+        if nmatches == len(properties.keys()):
+            yield (u,v)
+
+###
+###
+
+def edges_with_properties(network, properties, match_by = PropertiesFilter.all):
+    """
+    Get edges with given properties
+
+    Parameters
+    ---------
+    network : nx.MultiDiGraph
+        a street network
+
+    properties : dict(str : set)
+        properties of edges to filter by
+
+    match_by : int
+        . One of const.FILTER_PROPERTIES.
+
+    Returns
+    -------
+    generator
+        generator of edges (Edge)
+    """
+    if match_by == PropertiesFilter.at_least_one:
+        return edges_with_at_least_one_property(network, properties)
+
+    elif match_by == PropertiesFilter.all:
+        return edges_with_all_properties(network, properties)
+
+    else:
+        raise ValueError("Invalid 'match_by' value. Pick one of PropertiesFilter.{{{}}}.".format(PropertiesFilter.__order__))
