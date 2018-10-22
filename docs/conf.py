b28a289eceb7db87aa30eb8fbbf942c7780329d0
PedrosWits <ppintodasilva@gmail.com>
2018-10-22 12:44:23 +0100

    Added Rtree magicmock in readthedocs env

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,21 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import os
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
 
+if on_rtd:
+    import sys
+    from unittest.mock import MagicMock
+
+    class Mock(MagicMock):
+        @classmethod
+        def __getattr__(cls, name):
+            return MagicMock()
+
+    MOCK_MODULES = ['Rtree']
+
+    sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- Project information -----------------------------------------------------
 
