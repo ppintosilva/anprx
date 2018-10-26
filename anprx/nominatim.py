a514a5f8c620eb1aa19dedb8361dac5ac812b3cb
PedrosWits <ppintodasilva@gmail.com>
2018-10-26 16:13:26 +0100

    Added method and tests for address lookup based on OSM ids

--- anprx/nominatim.py
+++ anprx/nominatim.py
@@ -6,7 +6,9 @@
 # Web: https://github.com/pedroswits/anprx
 ################################################################################
 
+import numpy as np
 import osmnx as ox
+from .helpers import flatten_dict
 from collections import OrderedDict
 
 def search_address(address,
@@ -16,11 +18,11 @@ def search_address(address,
 
     Parameters
     ----------
-    address :
+    address : string
         Address to search for
 
-    email :
-        Valid email address in case you are making large number of
+    email : string
+        Valid email address in case you are making a large number of requests.
 
     Returns
     -------
@@ -37,9 +39,73 @@ def search_address(address,
 
     params['q'] = address
 
-    response_json = ox.nominatim_request(params = params)
+    response_json = ox.nominatim_request(params = params, type = 'search')
 
     ways = filter(lambda x: x['osm_type'] == "way", response_json)
     osmids = map(lambda x: int(x["osm_id"]), ways)
 
     return list(osmids)
+
+
+###
+###
+###
+
+
+def lookup_address(osmids,
+                   entity,
+                   drop_keys = ['place_id', 'license', 'osm_type',
+                                'osmid', ' lat', 'lon', 'display_name',
+                                'country', 'country_code', 'state',
+                                'state_district', 'county', 'city'],
+                   email = None):
+    """
+    Lookup the address of multiple OSM ids that share the same entity type.
+
+    Parameters
+    ----------
+    osmids : array-like
+        OSMids for address lookup. Hard limit of 50 ids as indicated in
+        wiki.openstreetmap.org/wiki/Nominatim
+
+    entity : string
+        OSM entity of osmids. Valid values are
+        'N' for Node, 'W' for Way and 'R' for Relation.
+
+    email : string
+        Valid email address in case you are making a large number of requests.
+
+    Returns
+    -------
+    details : list of dict
+        Address details for each input osmid
+    """
+
+    if entity not in {'N', 'W', 'R'}:
+        raise ValueError("Not valid OSM entity. Choose one of 'N', 'W' or 'R'")
+
+    if len(osmids) > 50:
+        raise ValueError("Nominatim supports a maximum of 50 osmids in a lookup request.")
+
+    params = OrderedDict()
+    params['format'] = "json"
+    params['address_details'] = 1
+
+    if email:
+        params['email'] = email
+
+    osmids = np.array(list(map(str, osmids)))
+    entities = np.repeat(entity, len(osmids))
+    params['osm_ids'] = ",".join(np.core.defchararray.add(entities, osmids))
+
+    response_json = ox.nominatim_request(params = params, type = 'lookup')
+
+    details = []
+    for dict_ in response_json:
+        flattened_dict = flatten_dict(dict_, inherit_parent_key = False)
+        if drop_keys:
+            for key in drop_keys:
+                flattened_dict.pop(key, None)
+        details.append(flattened_dict)
+
+    return details
--- tests/test_nominatim.py
+++ tests/test_nominatim.py
@@ -10,3 +10,28 @@ def test_nominatim_search_address_fail():
     result = anprx.search_address("AAAAAAAAAAA")
 
     assert result == []
+
+osmids = [37899441,
+          461119586,
+          4725926,
+          4692270,
+          4655478,
+          2544439,
+          31992849]
+
+def test_lookup_address_exceeds_limit():
+    with pytest.raises(ValueError):
+        anprx.lookup_address(osmids * 10, entity = 'W')
+
+def test_lookup_address_invalid_osm_entity():
+    with pytest.raises(ValueError):
+        anprx.lookup_address(osmids, entity = 'A')
+
+def test_lookup_address():
+    details = anprx.lookup_address(osmids, entity = 'W')
+
+    assert len(details) == len(osmids)
+
+    for dict_ in details:
+        assert len(dict_.keys()) > 0
+        assert {'road', 'importance', 'type', 'suburb'}.issubset(set(dict_.keys()))
