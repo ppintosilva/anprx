c83c25494d4fb5be553f06a6ed9b8c8881a5705e
Pedro Pinto da Silva <ppintodasilva@gmail.com>
2019-07-12 15:17:03 +0100

    Fixing as type to bool error

--- anprx/preprocessing.py
+++ anprx/preprocessing.py
@@ -79,10 +79,10 @@ def infer_road_attr(
 
     return pd.DataFrame({
         'direction'      : directions,
-        'both_directions': both_directions.astype('bool'),
+        'both_directions': both_directions.astype('int').astype('bool'),
         'ref'            : road_refs,
         'address'        : addresses,
-        'is_carpark'     : car_parks.astype('bool')
+        'is_carpark'     : car_parks.astype('int').astype('bool')
     })
 
 def filter_by_attr_distance(
